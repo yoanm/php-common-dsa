007ce24c2db07a1fcec9ecbf13fe870c88173e3d
Yoanm <4410697+yoanm@users.noreply.github.com>
2025-10-22 09:18:33 +0200

    Add Binary search algos

---
+++ src/Algorithm/BinarySearch.php
@@ -0,0 +1,156 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yoanm\CommonDSA\Algorithm;
+
+use ArrayAccess;
+
+class BinarySearch
+{
+    /**
+     * Find an index where value is equal to $target
+     *
+     *
+     * ### Time/Space complexity
+     * With:
+     * - 𝑛 equals the provided list length.
+     *
+     * TC: 𝑂(㏒ 𝑛) - classic for binary search
+     *
+     * SC: 𝑂(1) - Constant extra space
+     *
+     *
+     * @param array<int|float>|ArrayAccess<int, int|float> $list ⚠ Must be sorted in non-decreasing order (min→max)!<br>
+     *                                                           May contain duplicates.
+     * @param int|float $target
+     * @param int $lowIdx Lookup start index.<br>
+     *                    Default to 0 (head index).
+     * @param int|null $highIdx Lookup end index.<br>
+     *                          Default to tail index
+     *
+     * @return int Index where $target has been found, -1 if not found
+     */
+    public static function find(
+        array|ArrayAccess $list,
+        int|float $target,
+        int $lowIdx = 0,
+        int|null $highIdx = null,
+    ): int {
+        $highIdx ??= count($list) - 1;
+
+        while ($lowIdx <= $highIdx) {
+            // Prevent ($lowIdx + $highIdx) overflow !
+            $midIdx = $lowIdx + (($highIdx - $lowIdx) >> 1);
+
+            if ($list[$midIdx] < $target) {
+                // Current value is too small ? => Ignore left side and current
+                $lowIdx = $midIdx + 1;
+            } elseif ($list[$midIdx] > $target) {
+                // Current value is greater ? => Ignore right side and current
+                $highIdx = $midIdx - 1;
+            } else {
+                return $midIdx;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * Finds the index of left-most element which is greater than or equal to $target (=not strictly lower than $target)
+     *
+     *
+     * ### Time/Space complexity
+     * With:
+     * - 𝑛 equals the provided list length.
+     *
+     * TC: 𝑂(㏒ 𝑛) - classic for binary search
+     *
+     * SC: 𝑂(1) - Constant extra space
+     *
+     *
+     * @param array<int|float>|ArrayAccess<int, int|float> $list ⚠ Must be sorted in non-decreasing order (min→max)!<br>
+     *                                                           May contain duplicates.
+     * @param int|float $target
+     * @param int $lowIdx Lookup start index.<br>
+     *                    Default to 0 (head index).
+     * @param int|null $highIdx Lookup end index.<br>
+     *                          Default to $list length (=tail index + 1)
+     *
+     * @return int Index where $target can be inserted in $list while keeping it sorted.<br>
+     *             ⚠ Might be beyond $list current indexes if $target is greater than tail value !
+     */
+    public static function lowerBound(
+        array|ArrayAccess $list,
+        int|float $target,
+        int $lowIdx = 0,
+        int|null $highIdx = null,
+    ): int {
+        $highIdx ??= count($list);
+
+        while ($lowIdx < $highIdx) {
+            // Prevent ($lowIdx + $highIdx) overflow !
+            $midIdx = $lowIdx + (($highIdx - $lowIdx) >> 1);
+
+            if ($list[$midIdx] < $target) {
+                // Current value is too small ? => Ignore left side and current
+                $lowIdx = $midIdx + 1;
+            } else {
+                // Current value is either equal or greater ?
+                // => Ignore right side BUT keep current (might be the right index)
+                $highIdx = $midIdx;
+            }
+        }
+
+        return $lowIdx;
+    }
+
+    /**
+     * Finds the index of left-most element which is strictly greater than $target
+     *
+     *
+     * ### Time/Space complexity
+     * With:
+     * - 𝑛 equals the provided list length.
+     *
+     * TC: 𝑂(㏒ 𝑛) - classic for binary search
+     *
+     * SC: 𝑂(1) - Constant extra space
+     *
+     *
+     * @param array<int|float>|ArrayAccess<int, int|float> $list ⚠ Must be sorted in non-decreasing order (min→max)!<br>
+     *                                                           May contain duplicates.
+     * @param int|float $target
+     * @param int $lowIdx Lookup start index.<br>
+     *                    Default to 0 (head index).
+     * @param int|null $highIdx Lookup end index.<br>
+     *                          Default to $list length (=tail index + 1)
+     *
+     * @return int Index where $target can be inserted in $list while keeping it sorted.<br>
+     *             ⚠ Might be beyond $list current indexes if $target is greater than or equal to tail value !
+     */
+    public static function upperBound(
+        array|ArrayAccess $list,
+        int|float $target,
+        int $lowIdx = 0,
+        int|null $highIdx = null,
+    ): int {
+        $highIdx ??= count($list);
+
+        while ($lowIdx < $highIdx) {
+            // Prevent ($lowIdx + $highIdx) overflow !
+            $middleIdx = $lowIdx + (($highIdx - $lowIdx) >> 1);
+
+            if ($list[$middleIdx] <= $target) {
+                // Current value is either equal or too small ? => Ignore left side and current
+                $lowIdx = $middleIdx + 1;
+            } else {
+                // Current value is greater ? => Ignore right side BUT keep current (might be the right index)
+                $highIdx = $middleIdx;
+            }
+        }
+
+        return $lowIdx;
+    }
+}
